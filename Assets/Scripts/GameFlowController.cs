275129efbee454e39c8526c4dc64854a8e3d3b07
Kenneth Chan <wlchanas@connect.ust.hk>
2023-01-05 17:42:29 +0800

    Created scene for Flow

---
+++ Assets/Scripts/GameFlowController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameFlowController : MonoBehaviour
+{
+    [Header("Reference")] 
+    public Camera arCamera;
+    public ARController ARController;
+    
+    [Header("Debug logs")] [SerializeField] 
+    public Text DebugText;
+    public Text AreaText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
