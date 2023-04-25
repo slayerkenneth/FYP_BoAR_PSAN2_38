7b7b73868449871f1c3e96ea247cc7c09bf9c458
a-night-owl <donutouchvanessa@gmail.com>
2023-04-25 19:24:15 +0800

    one ui per chapter version, trying to switch sprites based on chapter in progress

---
+++ Assets/Dialogue/ImageSwitcher.cs
@@ -0,0 +1,30 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
+//using Yarn.Unity;
+//using Yarn;
+
+//public class ImageSwitcher : MonoBehaviour
+//{
+//    public Image image;
+//        Sprite newSprite = Resources.Load<Sprite>(image);
+//    //private DialogueRunner dialogueRunner;
+
+//    //void Start()
+//    //{
+//    //    dialogueRunner = GetComponent<DialogueRunner>();
+//    //    var changeImageDelegate = (System.Action<string[]>)ChangeImage;
+//    //    dialogueRunner.AddCommandHandler("changeImage", changeImageDelegate);
+//    //}
+
+//    //private void ChangeImage(string[] parameters)
+//    //{
+//    //    if (parameters.Length > 0)
+//    //    {
+//    //        string imageName = parameters[0];
+//    //        Sprite newSprite = Resources.Load<Sprite>(imageName);
+//    //        image.sprite = newSprite;
+//    //    }
+//    //}
+//}
